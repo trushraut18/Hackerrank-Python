81403da587e7b827461bd95cb4cd0bcdc1f4fc22
trushraut18 <59933562+trushraut18@users.noreply.github.com>
2020-03-31 16:47:51 +0530

    Python: Division

---
+++ PythonDivision.py
@@ -0,0 +1,11 @@
+from __future__ import division
+
+def func():
+  c=a//b
+  print(c)
+  c=a/b
+  print(c)
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    func()
